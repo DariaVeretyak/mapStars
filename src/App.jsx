5ee21fa8adac3af9cd610cd0abb6b68a3b4fa591
Daria Veretyak <dariaveretyak@gmail.com>
2022-12-03 03:09:09 +0200

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,79 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import classNames from 'classnames';
+import { LangProvider } from './context/LangProvider';
+import { Header } from './components/Header/Header';
+import { GetStartedDialog } from './components/GetStartedDialog';
+import { HomePage } from './components/HomePage';
+import { Technology } from './components/Technology';
+import { Advantages } from './components/Advantages/Advantages';
+import { HowWork } from './components/HowWork/HowWork';
+import { Services } from './components/Services';
+import { Packages } from './components/Packages';
+import { Examples } from './components/Examples';
+import { Awards } from './components/Awards/Awards';
+import { UsefulInfo } from './components/UsefulInfo';
+import { SettingUp } from './components/SettingUp';
+import { Contacts } from './components/Contacts/Contacts';
+import { Footer } from './components/Footer/Footer';
 import './App.scss';
 
-export const App = () => (
-  <div className="starter">
-    React starter pack
-  </div>
-);
+const App = () => {
+  const [openDialog, setOpenDialog] = useState(false);
+
+  const techRef = useRef(null);
+  const advantagesRef = useRef(null);
+  const howWorkRef = useRef(null);
+  const packagesRef = useRef(null);
+  const contactRef = useRef(null);
+
+  const getStarted = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+    setOpenDialog(true);
+  };
+
+  const closeDialog = () => {
+    setOpenDialog(false);
+  };
+
+  return (
+    <LangProvider>
+      <div
+        className={
+          classNames(`App + ${openDialog
+            ? 'App--dialog'
+            : ''
+          }`)
+        }
+      >
+        <Header
+          getStarted={getStarted}
+          techRef={techRef}
+          advantagesRef={advantagesRef}
+          howWorkRef={howWorkRef}
+          packagesRef={packagesRef}
+          contactRef={contactRef}
+        />
+
+        {openDialog && <GetStartedDialog closeDialog={closeDialog} />}
+
+        <div className="App__mainBlock">
+          <HomePage getStarted={getStarted} />
+          <Technology techRef={techRef} />
+          <Advantages advantagesRef={advantagesRef} />
+          <HowWork howWorkRef={howWorkRef} />
+          <Services />
+          <UsefulInfo />
+          <Packages getStarted={getStarted} packagesRef={packagesRef} />
+          <Examples />
+          <Awards />
+          <SettingUp />
+          <Contacts getStarted={getStarted} contactRef={contactRef} />
+        </div>
+
+        <Footer />
+      </div>
+    </LangProvider>
+  );
+};
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import { App } from './App';
+import { HashRouter } from 'react-router-dom';
+import App from './App';
+
+import './index.scss';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <HashRouter>
     <App />
-  </React.StrictMode>,
+  </HashRouter>,
   document.getElementById('root'),
 );
