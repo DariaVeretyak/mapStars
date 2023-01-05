4fe6dbad1198f70b78e6087939edcbff45d868db
Daria Veretyak <dariaveretyak@gmail.com>
2023-01-05 23:00:06 +0200

    new

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,17 @@
-import { React } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from 'react-router-dom';
 
 import App from "./App";
 import './index.scss';
 
-const container = document.getElementById("root");
-const root = createRoot(container);
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
